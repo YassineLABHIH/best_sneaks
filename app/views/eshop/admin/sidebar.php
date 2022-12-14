0e4bfc71c94528ebf24fcec8306f38a0b0e590d7
Yassine.L <y.labhih@gmail.com>
2022-12-14 13:12:32 +0100

    change dashboard sidebar

--- app/views/eshop/admin/sidebar.php
+++ app/views/eshop/admin/sidebar.php
@@ -8,20 +8,78 @@
               <ul class="sidebar-menu" id="nav-accordion">
               
               	  <p class="centered"><a href="profile.html"><img src="<?= ASSETS . THEME ?>admin/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
-              	  <h5 class="centered">Marcel Newman</h5>
+              	  <h5 class="centered"><?= $_SESSION['name'] ?></h5>
+                    <h6 class="centered"><?= $_SESSION['email'] ?></h6>
               	  	
 
                   <li class="sub-menu">
                       <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>UI Elements</span>
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/products" >
+                          <i class="fa fa-barcode"></i>
+                          <span>Products</span>
                       </a>
                       <ul class="sub">
-                          <li><a  href="general.html">General</a></li>
-                          <li><a  href="buttons.html">Buttons</a></li>
-                          <li><a  href="panels.html">Panels</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/add">Add New Product</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/edit">Edit Product</a></li>
+                          <li><a  href="<?= ROOT ?>admin/products/delete">Delete Product</a></li>
                       </ul>
                   </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/categories">
+                          <i class="fa fa-list"></i>
+                          <span>Categories</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="<?= ROOT ?>admin/categories/add">Add New Category</a></li>
+                          <li><a  href="<?= ROOT ?>admin/categories/edit">Edit Category</a></li>
+                          <li><a  href="<?= ROOT ?>admin/categories/delete">Delete Category</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/products" >
+                          <i class="fa fa-reorder"></i>
+                          <span>Orders</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/settings">
+                          <i class="fa fa-cogs"></i>
+                          <span>Settings</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="<?= ROOT ?>admin/settings/slider">Slider Images</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/users">
+                          <i class="fa fa-user"></i>
+                          <span>Users</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="<?= ROOT ?>admin/users/customers">Customers</a></li>
+                          <li><a  href="<?= ROOT ?>admin/users/admins">Admins</a></li>
+
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="<?= ROOT ?>admin/backup">
+                          <i class="fa fa-hdd-o"></i>
+                          <span>Website Backup</span>
+                      </a>
+                  </li>
+
 
               </ul>
               <!-- sidebar menu end-->
